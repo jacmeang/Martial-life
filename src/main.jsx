d0b0fe5746b272a8668217f4ef92768514e428e1
jacmeang <jacmeang@gmail.com>
2026-05-07 00:06:54 +0900

    Delete src/main.jsx

--- src/main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import MartialLife from '../martial-life.jsx'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <MartialLife />
-  </React.StrictMode>
-)
