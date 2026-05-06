dc141e08730b1e9f273e723a6f0649de438b24d0
jacmeang <jacmeang@gmail.com>
2026-05-07 00:03:24 +0900

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import MartialLife from '../martial-life.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <MartialLife />
+  </React.StrictMode>
+)
